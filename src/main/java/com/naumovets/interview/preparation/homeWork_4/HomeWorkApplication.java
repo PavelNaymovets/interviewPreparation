fa4b1ba38c61ee737a6694d03ab7c9b494b29448
Pavel Naumovets <pavel.naumovets@mail.ru>
2023-04-25 19:30:20 +0300

    Homework 4. Initial commit

---
+++ src/main/java/com/naumovets/interview/preparation/homeWork_4/HomeWorkApplication.java
@@ -0,0 +1,21 @@
+package com.naumovets.interview.preparation.homeWork_4;
+
+import com.naumovets.interview.preparation.homeWork_3.task_1.Ball;
+import com.naumovets.interview.preparation.homeWork_3.task_1.Player;
+import com.naumovets.interview.preparation.homeWork_3.task_2.Counter;
+import com.naumovets.interview.preparation.homeWork_3.task_2.ThreadSafeResource;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class HomeWorkApplication {
+    public static void main(String[] args) {
+        //=====================================================
+        // Задание 1.
+        //=====================================================
+
+        /*
+         Задание реализовано в пекедже task_1.
+        */
+
+    }
+}
