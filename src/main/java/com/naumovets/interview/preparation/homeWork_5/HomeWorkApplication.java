104434d340b9ad6c6a0a676857b924747f903986
Pavel Naumovets <pavel.naumovets@mail.ru>
2023-04-28 15:14:27 +0300

    Homework 5. Initial commit

---
+++ src/main/java/com/naumovets/interview/preparation/homeWork_5/HomeWorkApplication.java
@@ -0,0 +1,16 @@
+package com.naumovets.interview.preparation.homeWork_5;
+
+public class HomeWorkApplication {
+    public static void main(String[] args) {
+        //=====================================================
+        // Задание 1.
+        //=====================================================
+
+        /*
+         Задание реализовано в пекедже task_1.
+
+         Комментарии:
+         *
+        */
+    }
+}
