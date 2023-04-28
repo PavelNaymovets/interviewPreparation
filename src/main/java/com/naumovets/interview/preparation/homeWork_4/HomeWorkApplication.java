c226fb47aaf11f0b276e433a6fe78465a8f7fb4c
Pavel Naumovets <pavel.naumovets@mail.ru>
2023-04-28 13:12:49 +0300

    Homework 4. Initial commit

--- src/main/java/com/naumovets/interview/preparation/homeWork_4/HomeWorkApplication.java
+++ src/main/java/com/naumovets/interview/preparation/homeWork_4/HomeWorkApplication.java
@@ -1,12 +1,5 @@
 package com.naumovets.interview.preparation.homeWork_4;
 
-import com.naumovets.interview.preparation.homeWork_3.task_1.Ball;
-import com.naumovets.interview.preparation.homeWork_3.task_1.Player;
-import com.naumovets.interview.preparation.homeWork_3.task_2.Counter;
-import com.naumovets.interview.preparation.homeWork_3.task_2.ThreadSafeResource;
-
-import java.util.concurrent.locks.ReentrantLock;
-
 public class HomeWorkApplication {
     public static void main(String[] args) {
         //=====================================================
@@ -15,7 +8,9 @@ public class HomeWorkApplication {
 
         /*
          Задание реализовано в пекедже task_1.
-        */
 
+         Комментарии:
+         * В пекедже task_1 содержится файл query.sql в котором содержатся все необходимые запросы к бд согласно заданию.
+        */
     }
 }
